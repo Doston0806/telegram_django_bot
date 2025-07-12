2abb63c2f9c1ec8b4b0a88b7e26e161e252f0646
Aliboyev Doston <aliboyevdoston777@gmail.com>
2025-07-12 15:24:23 +0500

    wehbook

--- telegram_bot/set_webhook.py
+++ telegram_bot/set_webhook.py
@@ -1,6 +1,11 @@
 # telegram_bot/set_webhook.py
 import asyncio
+import os
+import sys
 from aiogram import Bot
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from sozlamalar import BOT_TOKEN
 
 bot = Bot(token=BOT_TOKEN)
