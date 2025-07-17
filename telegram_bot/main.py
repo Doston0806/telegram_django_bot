ab6b69c2348c79cf9778a6286955bd681b32a0bc
Aliboyev Doston <aliboyevdoston777@gmail.com>
2025-07-17 16:37:28 +0500

    add

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -1,23 +1,26 @@
 import os
-import django
 import sys
+import django
+import asyncio
+from dotenv import load_dotenv
 
+load_dotenv()
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
-import asyncio
+
 from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
 from handlers import router
 from scheduler import start_scheduler
-import os
-from dotenv import load_dotenv
-load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+if not BOT_TOKEN:
+    raise ValueError("❌ BOT_TOKEN topilmadi. .env faylga to‘g‘ri yozilganiga ishonch hosil qiling.")
 
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher()
 
 async def main():
@@ -26,5 +29,4 @@ async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-   asyncio.run(main())
-
+    asyncio.run(main())
