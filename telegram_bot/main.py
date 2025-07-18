887e7dd25956470e9492335279b88fa00f8607c2
Aliboyev Doston <aliboyevdoston777@gmail.com>
2025-07-18 17:42:30 +0500

    add new4

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -4,24 +4,25 @@ import asyncio
 import os
 import sys
 import django
-from dotenv import load_dotenv
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-load_dotenv(os.path.join(BASE_DIR, ".env"))
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# from dotenv import load_dotenv
+#
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# load_dotenv(os.path.join(BASE_DIR, ".env"))
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django.setup()
 
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
+from telegram_bot.sozlamalar import BOT_TOKEN
 
 from telegram_bot.handlers import router  # sizning handlerlaringiz shu yerda bo‘lishi kerak
 
 # Tokenni olish
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN environmentda yo'q!")
+# BOT_TOKEN = os.getenv("BOT_TOKEN")
+# if not BOT_TOKEN:
+#     raise ValueError("BOT_TOKEN environmentda yo'q!")
 
 # Bot va dispatcher
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
