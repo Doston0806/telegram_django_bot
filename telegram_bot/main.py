9a44e4259de6186df2a8035f85687691f702a110
Aliboyev Doston <aliboyevdoston777@gmail.com>
2025-07-18 16:54:21 +0500

    add new

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -1,42 +1,47 @@
 # telegram_bot/main.py
+
 import os
+import sys
 import django
 from dotenv import load_dotenv
 
-from aiogram import Bot, Dispatcher
-from aiogram.enums import ParseMode
-from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
-
-from telegram_bot.handlers import router
-
-import sys
-from aiohttp import web
+load_dotenv()
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django.setup()
-load_dotenv()
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.types import Update
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+from aiogram.client.default import DefaultBotProperties
+
+from aiohttp import web
+from telegram_bot.handlers import router
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
-bot = Bot(token=BOT_TOKEN, default=ParseMode.HTML)
-dp = Dispatcher(storage=MemoryStorage())
+bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher()
 dp.include_router(router)
 
-async def on_startup(bot: Bot):
+async def on_startup(app):
     await bot.set_webhook(WEBHOOK_URL)
 
-async def on_shutdown(bot: Bot):
+async def on_shutdown(app):
     await bot.delete_webhook()
 
-async def start_webhook(request):
-    # Bu endpoint faqat POST request uchun
-    return await SimpleRequestHandler(
-        dispatcher=dp,
-        bot=bot,
-        handle_in_background=False,
-    ).handler(request)
+def create_app():
+    app = web.Application()
+    app.on_startup.append(on_startup)
+    app.on_shutdown.append(on_shutdown)
 
-# Django URLConf orqali start_webhook view sifatida ishlatiladi
+    SimpleRequestHandler(dispatcher=dp, bot=bot).register(app, path="/webhook/")
+    setup_application(app, dp, bot=bot)
+
+    return app
+
+if __name__ == "__main__":
+    web.run_app(create_app(), port=8000)
